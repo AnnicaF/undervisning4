40bd6f852be0a9cd4a51375f82b1301f34dbb145
Annica <annif011098@gmail.com>
2024-02-20 11:25:27 +0100

    routing

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAW7W8gW2fT7usjtS6LVu03YtnaplsrIhs",
+  authDomain: "undervisning4-5df20.firebaseapp.com",
+  projectId: "undervisning4-5df20",
+  storageBucket: "undervisning4-5df20.appspot.com",
+  messagingSenderId: "1085383590933",
+  appId: "1:1085383590933:web:56fc5a46720dee457d3a1c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,19 @@
+<script>
+import { useRoute, useRouter } from "vue-router";
+const route = useRoute();
+const router = useRouter();
+const routes = router.getRoutes();
+
+console.log(routes);
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+  <menu>
+    <li v-for="r in roites" :key="r.path">
+    <RouterLink :to="r.path">{{ r.meta.title }}</RouterLink></li>
+  </menu>
   </div>
 </template>
 
